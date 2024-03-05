7f009269ef5957bc4f60f833d2b3c803998cc684
Azy BK <kumbaraazy@gmail.com>
2024-03-05 22:54:44 +0700

    header

--- react-redux/src/components/Header.jsx
+++ react-redux/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import Cart from '../assets/cart.svg'
+
 const Header = () => {
     return (
         <header className="bg-blue-700">
@@ -5,7 +7,7 @@ const Header = () => {
                 <div className="flex items-center justify-between h-20">
                     <h1 className="text-3xl font-bold text-gray-100">My E-Commerce</h1>
                     <button type="button" className="relative rounded-full bg-blue-800 p-2 text-gray-100">
-                        Cart
+                        <img src={Cart} alt='cart' className='w-6 h-6' />
                     </button>
                 </div>
             </div>
--- react-redux/src/features/productlist/ProductList.jsx
+++ react-redux/src/features/productlist/ProductList.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react"
+
 const ProductList = () => {
+    const [products, setProducts] = useState([])
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchProducts = async() => {
+            setLoading(true)
+            try {
+                const response = await fetch('https://fakestoreapi.com/products')
+                const data = response.json()
+                setProducts(data)
+
+            } catch(error) {
+                console.error(error)
+            
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchProducts()
+    }, [])
+
+    console.log(products)
+
     return (
         <div>Product List</div>
     )
