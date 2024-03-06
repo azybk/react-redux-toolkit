d12315ab9a996a9737276742cd02881462b4ea8e
Azy BK <kumbaraazy@gmail.com>
2024-03-06 21:55:08 +0700

    product list

--- react-redux/src/features/productlist/ProductList.jsx
+++ react-redux/src/features/productlist/ProductList.jsx
@@ -9,7 +9,7 @@ const ProductList = () => {
             setLoading(true)
             try {
                 const response = await fetch('https://fakestoreapi.com/products')
-                const data = response.json()
+                const data = await response.json()
                 setProducts(data)
 
             } catch(error) {
@@ -19,14 +19,36 @@ const ProductList = () => {
                 setLoading(false)
             }
         }
-
+        
         fetchProducts()
     }, [])
 
-    console.log(products)
-
     return (
-        <div>Product List</div>
+        <div className="w-full h-full grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 py-4">
+             {
+                products.map((product) => {
+                    return(
+                        <div key={product.id} className="bg-white rounded-xl border shadow p-4 w-full">
+                            <div className="group relative w-[80%] h-[300px] mx-auto overflow-hidden">
+                                <img src={product.image} alt={product.title} className="w-full h-full object-contain group-hover:scale-110 transition-all duration-500 ease-in-out" />
+                            </div>
+
+                            <div className="flex flex-col gap-6 mt-8">
+                                <button 
+                                    type="button" 
+                                    className="bg-blue-700 text-white hover:bg-blue-800 rounded-lg text-sm py-3 px-8"
+                                >
+                                    BUY NOW - ${product.price}
+                                </button>
+                            <h3 className="font-bold">{product.title}</h3>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        
+        </div>
+
     )
 }
 
